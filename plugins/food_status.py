bcd832bf2f411b6fc56e4860b907473578a9f938
chuangcaleb <balechuang@gmail.com>
2022-06-03 17:02:16 +0100

    feat + refac: add milk + cake, auto namespace

--- plugins/food_status.py
+++ plugins/food_status.py
@@ -3,10 +3,19 @@
 from beet import Context, Function, Advancement
 import yaml
 
+SCOREBOARD_FOODS = {
+    "cake": "minecraft.custom:minecraft.eat_cake_slice",
+    "milk_bucket": "minecraft.used:minecraft.milk_bucket"
+}
+
 
 def generate_function(food, effect_list):
 
-    function_body = [f"advancement revoke @s only cchesed:foodstatus/{food}"]
+    if food in SCOREBOARD_FOODS.keys():
+        function_body = [f"scoreboard players set @s cc.fs.{food} 0"]
+    else:
+        function_body = [
+            f"advancement revoke @s only food_status_effects:{food}"]
 
     for effect in effect_list:
         # for effect in effect_list:
@@ -23,22 +32,53 @@ def beet_default(ctx: Context):
 
 def generate_files(ctx: Context):
 
+    ctx.generate(
+        "init",
+        Function(
+            [f"scoreboard objectives add cc.fs.{food} {SCOREBOARD_FOODS[food]}"
+             for food in SCOREBOARD_FOODS],
+            prepend_tags=["minecraft:load"]
+        )
+    )
+    ctx.generate(
+        "uninstall",
+        Function(
+            [f"scoreboard objectives remove cc.fs.{food}"
+             for food in SCOREBOARD_FOODS],
+        )
+    )
+
     with open('data.yml', 'r') as file:
         data = yaml.safe_load(file)
-        # print(data)
+
+        tick_check = []
 
         for food, effect_list in data.items():
-            # for food, effect_list in data.FOOD_STATUS_CONFIG.items():
 
-            filename = f"{ctx.project_author}:foodstatus"
-
-            ctx.data[f"{filename}/{food}"] = Function(
-                generate_function(food, effect_list)
+            ctx.generate(
+                f"food_effect/{food}",
+                Function(
+                    generate_function(food, effect_list),
+                )
             )
 
-            ctx.data[f"{filename}/{food}"] = Advancement(
-                ctx.template.render(
-                    "consume_food.json",
-                    food=food
+            if food in SCOREBOARD_FOODS:
+                tick_check.append(
+                    f"execute as @a if score @s cc.fs.{food} matches 1.. run function food_status_effects:food_effect/{food}")
+            else:
+                ctx.generate(
+                    food,
+                    Advancement(ctx.template.render(
+                        "consume_food.json",
+                        food=food
+                    ))
                 )
+
+        if tick_check:
+            ctx.generate(
+                f"tick",
+                Function(
+                    tick_check,
+                    prepend_tags=["minecraft:tick"]
+                ),
             )
